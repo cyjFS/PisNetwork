a256df0e0679e62cb7a75ce89265a62ed812bc73
cyjFS <693508027@qq.com>
2015-08-12 15:26:10 +0800

    Initial Commit

---
+++ PisNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PisNetwork
+//
+//  Created by newegg on 15/8/12.
+//  Copyright (c) 2015年 newegg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
